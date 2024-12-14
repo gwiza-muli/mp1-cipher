b4f20b6d8529ff22d40753f28925c38cd3ff783a
sheilla <muligand@grinnell.edu>
2024-12-13 21:12:52 -0600

    fixing main cipher.java

--- src/main/java/edu/grinnell/csc207/Cipher.java
+++ src/main/java/edu/grinnell/csc207/Cipher.java
@@ -20,7 +20,7 @@ public class Cipher {
    *        cipher (caesar or vigenere).
    */
   public static void main(String[] args) {
-    PrintWriter pen = new PrintWriter(System.out, true);
+   // PrintWriter pen = new PrintWriter(System.out, true);
     String action = "";
     String key = "";
     String cipher = "";
@@ -30,71 +30,68 @@ public class Cipher {
       return;
     } else {
       for (int i = 0; i < args.length; i++) {
-        if (args[i].length() != 0 && args[i].charAt(0) == '-') {
-          if ((args[i].equals("-encode")) || args[i].equals("-decode")) {
+        String currArg = args[i];
+        if (currArg.length() != 0 && currArg.charAt(0) == '-') {
+          if ((currArg.equals("-encode")) || currArg.equals("-decode")) {
             if (action.isEmpty()) {
-              action = CipherUtils.dashRemover(args[i]);
+              action = CipherUtils.dashRemover(currArg);
             } else {
               System.err.println("Error: You can't give more than one action.");
               return;
             } // if
-          } else if ((args[i].equals("-vigenere")) || args[i].equals("-caesar")) {
+          } else if ((currArg.equals("-caesar")) || currArg.equals("-vigenere")) {
             if (cipher.isEmpty()) {
-              cipher = CipherUtils.dashRemover(args[i]);
+              cipher = CipherUtils.dashRemover(currArg);
             } else {
-              System.err.println("Error: You can't give more than one cypher type.");
+              System.err.println("Error: You can't give more than one cipher type.");
               return;
             } // else
           } // if
         } else {
-          if (!(CipherUtils.alphaNumericChecker(args[i]))) {
+          if (!(CipherUtils.alphaNumericChecker(currArg))) {
             System.err.println("Error: string must be a lowercase letter.");
             return;
           } else if (word.isEmpty()) {
-            word = args[i];
+            word = currArg;
           } else if (key.isEmpty()) {
             if (cipher.equals("caesar") || cipher.equals("vigenere")) {
-              if (!args[i].isEmpty()) {
-                key = args[i];
+              if (!currArg.isEmpty()) {
+                key = currArg;
               } else {
                 System.err.println("Error: Empty keys are not permitted");
                 return;
               } // if
             } else {
-              key = args[i];
+              key = currArg;
             } // else
           } // else if
         } // if
       } // for
-
       if (cipher.equals("caesar") && ((key.length() == 1))) {
-
         if (action.equals("encode")) {
-          System.out.printf("%s", CipherUtils.caesarEncrypt(word, key.charAt(0)));
+          System.out.println( CipherUtils.caesarEncrypt(word, key.charAt(0)));
         } else if (action.equals("decode")) {
-          System.out.printf("%s", CipherUtils.caesarDecrypt(word, key.charAt(0)));
+          System.out.println(CipherUtils.caesarDecrypt(word, key.charAt(0)));
         } else {
-          System.err
-              .println("No valid action specified.  Legal values are '-encode' and '-decode'");
+          System.err.println("No valid action specified.  Legal values are '-encode' and '-decode'");
         } // else
-      } else if (cipher.equals("caesar") && (!(key.length() == 1))) {
+      } else if (cipher.equals("caesar") && (key.length() != 1)) {
         System.err.println("Error: Your key must be 1 character long for Caesar Ciphers.");
         return;
       } // if
       if (cipher.equals("vigenere")) {
         if (action.equals("encode")) {
-          System.out.printf("%s", CipherUtils.vigenereEncrypt(word, key));
+          System.out.println(CipherUtils.vigenereEncrypt(word, key));
         } else if (action.equals("decode")) {
-          System.out.printf("%s", CipherUtils.vigenereDecrypt(word, key));
+          System.out.println(CipherUtils.vigenereDecrypt(word, key));
         } else {
-          System.err
-              .println("No valid action specified.  Legal values are '-encode' and '-decode'");
+          System.err.println("No valid action specified.  Legal values are '-encode' and '-decode'");
           return;
         } // if
       } // if
 
     } // else
 
-    pen.close();
+   // pen.close();
   } // main
 } // Cipher
