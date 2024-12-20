9453b34aa2073aa1634a72b7be19df740986428a
sheilla <muligand@grinnell.edu>
2024-12-20 13:38:26 -0600

    ensuring files produce output

--- src/main/java/edu/grinnell/csc207/main/Cipher.java
+++ src/main/java/edu/grinnell/csc207/main/Cipher.java
@@ -1,4 +1,5 @@
 package edu.grinnell.csc207.main;
+
 import edu.grinnell.csc207.util.CipherUtils;
 
 /**
@@ -19,20 +20,14 @@ public class Cipher {
    *        cipher (caesar or vigenere).
    */
   public static void main(String[] args) {
-   // PrintWriter pen = new PrintWriter(System.out, true);
+    // PrintWriter pen = new PrintWriter(System.out, true);
     String action = "";
     String key = "";
     String cipher = "";
     String word = "";
-
-    System.out.println("Action: " + action);
-    System.out.println("Cipher: " + cipher);
-    System.out.println("Word: " + word);
-    System.out.println("Key: " + key);
-
     if (args.length != EXPECTED_NUM_PARAMS) {
-      System.err.println("Error: Invalid number of parameters. Expected 4 parameters, received "
-          + args.length);
+      System.err.println(
+          "Error: Invalid number of parameters. Expected 4 parameters, received " + args.length);
       return;
     } else {
       for (int i = 0; i < args.length; i++) {
@@ -57,28 +52,30 @@ public class Cipher {
           if (word.isEmpty()) {
             word = currArg;
           } else if (key.isEmpty()) {
-                key = currArg;}
-               else {
-                System.err.println("Error: Empty keys are not permitted");
-                return;
-              } // if
-            } //else 
-          } // for
-
-          if(action.isEmpty() | cipher.isEmpty() || word.isEmpty() || key.isEmpty()){
-            System.err.println("missing arguments");
+            key = currArg;
+          } else {
+            System.err.println("Error: Empty keys are not permitted");
             return;
-          }
+          } // if
+        } // else
+      } // for
+
+      if (action.isEmpty() | cipher.isEmpty() || word.isEmpty() || key.isEmpty()) {
+        System.err.println("missing arguments");
+        return;
+      } //if
       if (cipher.equals("caesar") && ((key.length() == 1))) {
         if (action.equals("encode")) {
           System.out.print(CipherUtils.caesarEncrypt(word, key.charAt(0)));
+          System.out.print("\n");
           System.out.flush();
         } else if (action.equals("decode")) {
           System.out.print(CipherUtils.caesarDecrypt(word, key.charAt(0)));
+          System.out.print("\n");
           System.out.flush();
         } else {
-          System.err.println("No valid action specified."
-              + "Legal values are '-encode' and '-decode'");
+          System.err
+              .println("No valid action specified." + "Legal values are '-encode' and '-decode'");
         } // else
       } else if (cipher.equals("caesar") && (key.length() != 1)) {
         System.err.println("Error: Your key must be 1 character long for Caesar Ciphers.");
@@ -87,19 +84,21 @@ public class Cipher {
       if (cipher.equals("vigenere")) {
         if (action.equals("encode")) {
           System.out.print(CipherUtils.vigenereEncrypt(word, key));
+          System.out.print("\n");
           System.out.flush();
         } else if (action.equals("decode")) {
           System.out.print(CipherUtils.vigenereDecrypt(word, key));
+          System.out.print("\n");
           System.out.flush();
         } else {
-          System.err.println("No valid action specified."
-              + "Legal values are '-encode' and '-decode'");
+          System.err
+              .println("No valid action specified." + "Legal values are '-encode' and '-decode'");
           return;
         } // if
       } // if
 
     } // else
 
-   // pen.close();
+    // pen.close();
   } // main
 } // Cipher
